8f92c3b1e8f61e8829b9bd8eea970ff5b91b1ec6
jdrodriguez1000 <jdrodriguez1000@gmail.com>
2022-07-26 19:41:22 -0500

    Creacion de los archivos covid_original y dist_vacunas_original

---
+++ 0 Extract.R
@@ -0,0 +1,14 @@
+# ------- CARGA DE ARCHIVOS DE INFORMACION --------
+
+# Activacion de librerias
+library(readr)
+library(dplyr)
+
+# Carga del set original con los datos de contagios de covid 19 en Colombia
+ruta_archivo <- file.choose()
+covid_original <- read_csv(ruta_archivo)
+
+# Carga del set original con los datos de distribucion de vacunas contra el covid 19 en Colombia
+ruta_archivo1 <- file.choose()
+dist_vacunas_original <- read_csv(ruta_archivo1) 
+
