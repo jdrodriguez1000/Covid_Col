68737dff9fdb69ec6f406b0f7a9be6e763be60be
jdrodriguez1000 <jdrodriguez1000@gmail.com>
2022-07-29 14:56:01 -0500

    Creacion del archivo y primeros graficos

---
+++ 4 Visualisation.R
@@ -0,0 +1,184 @@
+# Activacion de librerias
+library(lubridate)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(forcats)
+
+# Grafico de total de casos por año
+ggplot(casos_por_año, aes(año, total_casos)) +
+  geom_col(fill = "#D4E6F1") +
+  theme_classic() +
+  labs(title = "Casos por año",
+       x= "Años",
+       y ="Numero de casos",
+       caption = "INS Colombia") +
+  geom_text(aes(label = total_casos, vjust= 1.5, hjust=0.5))
+
+# Grafico de casos acumulados por año 
+ggplot(casos_por_año, aes(año, casos_acumulados)) +
+  geom_col(fill= "#D98880") +
+  theme_classic() +
+  labs(title = "Casos acumulados por año",
+       x= "Años",
+       y ="Numero de casos acumulados",
+       caption = "INS Colombia") +
+  geom_text(aes(label = casos_acumulados, vjust= 1.5, hjust=0.5))
+
+
+# Gráfico total casos por departamento
+ggplot(casos_año_dpto, aes(Departamento, Casos_acumulados)) +
+  geom_col(fill = "#BB8FCE") +
+  theme_classic() +
+  coord_cartesian(ylim = c(0, 2000000), expand = TRUE)+
+  theme(axis.text.x = element_text(angle = 90, size = 6)) +
+  labs(title = "Casos totales por departamento",
+       x= "Departamentos",
+       y ="Numero de casos",
+       caption = "INS Colombia") 
+
+#Grafico total de casos reportados por departamento y año
+ggplot(casos_año_dpto) +
+  geom_col(mapping = aes(x=Departamento, y=Casos_acumulados, fill= Año)) +
+  theme_classic() +
+  coord_cartesian(ylim = c(0, 1900000), expand = TRUE)+
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 6)) +
+  labs(title = "Casos totales por departamento",
+       x= "Departamentos",
+       y ="Numero de casos",
+       caption = "INS Colombia") 
+
+# Grafico de casos totales por dia
+ggplot(casos_dia, aes(Fecha, Casos_totales)) +
+  geom_line(color = "#6C3483") + 
+  theme_classic() +
+  labs(title = "Distribución de casos diarios",
+       x = "Fechas",
+       y = "Número de casos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) 
+
+
+# Gráfico caumulado de casos totales por dia
+ggplot(casos_dia, aes(Fecha, Casos_acumulados)) +
+  geom_line(color = "#6C3483") + 
+  theme_classic() +
+  labs(title = "Distribución de casos diarios",
+       x = "Fechas",
+       y = "Número de casos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) 
+
+# Grafico de casos totales por dia para un departamento
+ggplot(casos_dia_dpto, aes(Fecha_notificacion, Total_casos)) +
+  geom_line(color = "#16A085") +
+  theme_classic() +
+  labs(title = "Casos diarios departamento de Antioquia",
+       x = "Fechas",
+       y = "Casos registrados",
+       caption = "INS Colombia") + 
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7))   
+
+# Grafico de casos acumulados diarios para un departamento
+ggplot(casos_dia_dpto, aes(Fecha_notificacion, Casos_acumulados)) +
+  geom_line(color = "#16A085") +
+  theme_classic() +
+  labs(title = "Casos acumulados diarios departamento de Antioquia",
+       x = "Fechas",
+       y = "Casos acuumulados",
+       caption = "INS Colombia") + 
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7))   
+
+
+# grafico de casos diarios para un municipio
+ggplot(casos_dia_mpio, aes(Fecha_notificacion, Casos_registrados)) +
+  geom_line(color ="#D35400") +
+  theme_classic() +
+  labs(title = "Casos diarios registrados municipio de Sabaneta",
+       x = "Fechas",
+       y = "Casos registrados",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7))  
+
+# Gráfico de casos acumulados para un municipio
+ggplot(casos_dia_mpio, aes(Fecha_notificacion, Casos_acumulados)) +
+  geom_line(color ="#2471A3") +
+  theme_classic() +
+  labs(title = "Casos acumulados diarios municipio de Sabaneta",
+       x = "Fechas",
+       y = "Casos acumuluados",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) 
+
+
+# Grafico de muertes por año
+ggplot(muertes_año, aes(Año, Fallecimientos)) +
+  geom_col(fill = "#F06292") +
+  theme_bw() +
+  labs(title = "Numero de fallecimientos por año",
+       x = "Años",
+       y = "Fallecimientos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) +
+  geom_text(aes(label = Fallecimientos, vjust= 1.5, hjust=0.5))
+
+
+# Grafico de muertes acumuladas por año
+ggplot(muertes_año, aes(Año, Muertes_acumuladas)) +
+  geom_col(fill = "#F06292") +
+  theme_bw() +
+  labs(title = "Fallecimientos acumulados por año",
+       x = "Años",
+       y = "Fallecimientos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) +
+  geom_text(aes(label = Muertes_acumuladas, vjust= 1.5, hjust=0.5))
+
+
+# Grafico numero de muertes diarias
+ggplot(muertes_dia, aes(Fecha, Fallecimientos)) +
+  geom_line(color= "#CC0033") +
+  theme_bw() +
+  labs(title = "Comportamiento de muertes por dia",
+       x = "Fechas",
+       y = "Número de fallecimientos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7))
+
+# Grafico de muertes acumuladas diarias
+ggplot(muertes_dia, aes(Fecha, Muertes_acumuladas)) +
+  geom_line(color= "#CC0033") +
+  theme_bw() +
+  labs(title = "Comportamiento de muertes acumuladas por dia",
+       x = "Fechas",
+       y = "Fallecimientos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) 
+
+# Comportamiento de muertes por mes
+ggplot(muertes_mes, aes(año_mes, Fallecimientos)) +
+  geom_col(fill = "#90CAF9") +
+  theme_bw() + 
+  labs(title = "Comportamiento de muertes por mes",
+       x = "Meses",
+       y = "Fallecimientos",
+       caption = "INS Colombia") +
+  theme(axis.text.x = element_text(angle = 90, size = 7)) +
+  theme(axis.text.y = element_text(size = 7)) +
+  geom_text(aes(label = Fallecimientos, vjust= 1.5, hjust=0.5), size =3)
+
+
+
+
+  
